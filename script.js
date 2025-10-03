8e2ad424da26a01329a129cf2aab746f08e44cc7
MD FERDAUS ALAM <ferdausalam244@gmail.com>
2025-10-03 06:48:58 +0600

    Update script.js

--- script.js
+++ script.js
@@ -156,7 +156,7 @@ document.addEventListener("DOMContentLoaded", function() {
             const rows = parseCSV(csvText);
 
             // কলাম ইনডেক্স
-            const cols1 = [38, 39], cols2 = [41, 42], cols3 = [44, 45], 
+            const cols1 = [38, 39], cols2 = [41, 42], cols3 = [45, 46], 
                   cols4 = [0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35],
                   cols5 = [37, 38, 39, 41, 42, 44, 45, 46, 47], 
                   cols6 = [47, 48, 49, 50, 51, 52, 53, 54, 55, 56], 
@@ -214,4 +214,5 @@ document.addEventListener("DOMContentLoaded", function() {
             loadingElement.style.color = "#f87171";
         });
 });
+
 
